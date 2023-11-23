910348982a7e47bd1bd2b11faef0ec194e57b9a9
Jerry Balderas <balderasfam@gmail.com>
2023-11-23 15:09:58 -0600

    Added navbar injection and updated flexbox

---
+++ nav-injector.js
@@ -0,0 +1,30 @@
+
+let navbar = document.querySelector("#navbar");
+navbar.innerHTML = `
+    <ul>
+        <li class="nav-link">
+            <a href="index.html">Headings</a>
+        </li>
+        <li class="nav-link">
+            <a href="tables.html">Tables</a>
+        </li>
+        <li class="nav-link">
+            <a href="lists.html">Lists</a>
+        </li>
+        <li class="nav-link">
+            <a href="forms.html">Forms</a>
+        </li>
+        <li class="nav-link">
+            <a href="semantic-tags.html">Semantic Tags</a>
+        </li>
+        <li class="nav-link">
+            <a href="css-grid.html">CSS Grid</a>
+        </li>
+        <li class="nav-link">
+            <a href="flexbox.html">Flexbox</a>
+        </li>
+        <li class="nav-link">
+            <a href="array-higher-order-functions.html">Array Higher Order Functions</a>
+        </li>
+    </ul>
+`
